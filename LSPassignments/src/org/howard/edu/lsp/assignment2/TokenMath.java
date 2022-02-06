2c2c4027774c7f2161eaa721c5b7a5afd4bd4518
rjingwi <ryan.jingwi@gmail.com>
2022-02-06 15:20:46 -0500

    Just finished assignment 2

---
+++ LSPassignments/src/org/howard/edu/lsp/assignment2/TokenMath.java
@@ -0,0 +1,36 @@
+package org.howard.edu.lsp.assignment2;
+import java.util.Scanner;
+
+public class TokenMath {
+
+	public static void main(String[] args) {
+		boolean keepgoing = true;
+		while (keepgoing == true){
+			int sum = 0;
+			int product = 1;
+			System.out.println("String?");
+			Scanner scanner = new Scanner(System.in);
+			String input = scanner.nextLine();
+			if (input.equals("Goodbye")) {
+				keepgoing = false;
+			}
+			else{
+				String[] tokens = input.split(" ");
+				for(String i:tokens){
+					sum += Integer.valueOf(i);
+				}
+				for(String i:tokens) {
+					product = product*Integer.valueOf(i); 
+				}
+				
+				System.out.println("Tokens:");
+				for(String i:tokens) {
+					System.out.println(i);
+				}
+				System.out.println("Sum: " + sum);				
+				System.out.println("Product: "+ product);
+			}
+		}
+
+	}
+}
