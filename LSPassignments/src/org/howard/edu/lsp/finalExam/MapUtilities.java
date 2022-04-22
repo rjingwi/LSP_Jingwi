913b1e35ecb7361ff2bba1f0eefd932b48cd2603
rjingwi <rjingwi@rjingwi-XPS-15-7590>
2022-04-22 11:06:56 -0400

    Finished implementing

--- LSPassignments/src/org/howard/edu/lsp/finalExam/MapUtilities.java
+++ LSPassignments/src/org/howard/edu/lsp/finalExam/MapUtilities.java
@@ -7,6 +7,13 @@ import java.util.Set;
 public class MapUtilities {
 	
 	public static int commonKeyValuePairs(HashMap<String, String> map1,HashMap<String, String> map2) throws NullMapException {
+		if ((map1 == null) || (map2 == null)){
+			throw new NullMapException();
+		}
+		if(map1.isEmpty() || map2.isEmpty()) {
+			return 0;
+		}
+		
 		int result = 0;
 		Iterator<Entry<String,String>> iterator = map1.entrySet().iterator();
 		while (iterator.hasNext()) {
